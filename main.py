5d73432662db0b9bd19b9b783eab5c571dd9880c
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-02-19 17:16:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,25 +6,20 @@ import ImageProcessing
 import ImageDisplaying
 
 try:
-    def DisplayLines():
-        # https://geeksforgeeks.org/python-play-a-video-using-opencv/ lines 15 - 20
-        # Variable needed for displaying the video
-        videoIsPlaying = True
+    # https://geeksforgeeks.org/python-play-a-video-using-opencv/ lines 15 - 20
+    # Variable needed for displaying the video
+    videoIsPlaying = True
 
-        # Starts the video capture
-        video = cv.VideoCapture(0)
+    # Starts the video capture
+    video = cv.VideoCapture(0)
 
-        # While the video is playing, read the frame, process it & display it
-        while videoIsPlaying == True:
-            videoIsPlaying, frame = video.read()
-            ImageProcessing.processImage(frame)
-            ImageDisplaying.displayImage(frame)
-        # Destroys the program when exiting
-        cv.destroyAllWindows()
-
-
-    # Runs the program
-    DisplayLines()
+    # While the video is playing, read the frame, process it & display it
+    while videoIsPlaying == True:
+        videoIsPlaying, frame = video.read()
+        ImageProcessing.processImage(frame)
+        ImageDisplaying.displayImage(frame)
+    # Destroys the program when exiting
+    cv.destroyAllWindows()
 
 # Removes the error message when you stop the program
 except:
