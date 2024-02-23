c0aae98a35c8f402858a674da1d5c829b7a5444b
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-02-22 22:47:31 -0500

    Update ImageProcessing.py

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -8,27 +8,27 @@ def processImage(image):
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     gray_scale = cv.GaussianBlur(gray, (15, 15), 1)
     median_blur = cv.medianBlur(gray_scale, 5)
-    #https://github.com/adityagandhamal/road-lane-detection/blob/master/detection_on_vid.py Line 36
+    # https://github.com/adityagandhamal/road-lane-detection/blob/master/detection_on_vid.py Line 36
     canny_image = cv.Canny(median_blur, 25, 25)
 
     # Creates a mask around desired area
     # https://pyimagesearch.com/2021/01/19/image-masking-with-opencv/ Lines 20-26
     roi = np.zeros(image.shape[:2], dtype="uint8")
-    cv.rectangle(roi, (200, 200), (850, 850), 1, -1)
+    cv.rectangle(roi, (20, 20), (350, 350), 1, -1)
     mask = cv.bitwise_and(canny_image, canny_image, mask=roi)
     # Displays the mask
-    cv.rectangle(image, (200, 200), (850, 850), (255, 0, 0), 5)
-    #Creates the contours
-    #https://www.geeksforgeeks.org/find-and-draw-contours-using-opencv-python/
+    cv.rectangle(image, (20, 20), (350, 350), (255, 0, 0), 5)
+    # Creates the contours
+    # https://www.geeksforgeeks.org/find-and-draw-contours-using-opencv-python/
     contour = contours, hierarchy = cv.findContours(mask,
-                                           cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+                                                    cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     # Prevents program from crashing if no lines detected
-    #https://www.tutorialspoint.com/how-to-compute-image-moments-in-opencv-python
+    # https://www.tutorialspoint.com/how-to-compute-image-moments-in-opencv-python
     if contour is not None:
-        #Displays the lines
+        # Displays the lines
         cv.drawContours(image, contours, -1, (255, 0, 0), 5)
 
-        #https://www.tutorialspoint.com/how-to-compute-image-moments-in-opencv-python Example 2
+        # https://www.tutorialspoint.com/how-to-compute-image-moments-in-opencv-python Example 2
         for i, point1 in enumerate(contours):
             for j, point2 in enumerate(contours):
                 x1, y1 = point1[0, 0]
@@ -39,4 +39,7 @@ def processImage(image):
                 # Calculates and displays the centerline
                 center_dots = cv.line(image, (midpoint1, midpoint2), (midpoint1, midpoint2), (0, 255, 0), 10)
 
-      
+                #https://www.tutorialspoint.com/how-to-draw-polylines-on-an-image-in-opencv-using-python
+                points = np.array([[100,20],[320,400]])
+                pts = points.reshape(-1, 1, 2)
+                cv.polylines(image, [pts], isClosed=False, color=(0,0,0), thickness = 2)
