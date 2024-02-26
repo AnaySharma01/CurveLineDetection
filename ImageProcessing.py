b517fd67f3ae95a2889d6187785376f454d44e0d
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-02-25 21:16:06 -0500

    Update ImageProcessing.py

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -2,6 +2,7 @@
 import cv2 as cv
 import numpy as np
 
+
 def processImage(image):
     # Applies gaussian blur, median blur, and canny edge detection on the image
     # https://github.com/adityagandhamal/road-lane-detection/blob/master/detection_on_vid.py Lines 35-38
@@ -19,27 +20,27 @@ def processImage(image):
     # Displays the mask
     cv.rectangle(image, (200, 200), (850, 850), (255, 0, 0), 5)
 
-    #Detects the contours
-    ##https://www.tutorialspoint.com/opencv_python/opencv_python_image_contours.htm Line 10
+    # Detects the contours
+    # https://www.tutorialspoint.com/opencv_python/opencv_python_image_contours.htm Line 10
     contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
 
     # Prevents program from crashing if no contours detected
     if len(contours) > 0:
-        # Displays the lines
-        #https://www.tutorialspoint.com/opencv_python/opencv_python_image_contours.htm Line 15
+        # Displays the contours
+        # https://www.tutorialspoint.com/opencv_python/opencv_python_image_contours.htm Line 15
         cv.drawContours(image, contours, -1, (0, 255, 0), 5)
 
-        #Finds minimum length of contours
-        #https://www.geeksforgeeks.org/python-length-of-shortest-string-in-string-list/ Method 1 Line 13
+        # Finds minimum length of contours
+        # https://www.geeksforgeeks.org/python-length-of-shortest-string-in-string-list/ Method 1 Line 13
         min_length = min(len(cnt) for cnt in contours)
 
-        #Calculates the average of the contour points
-        #https://www.geeksforgeeks.org/numpy-mean-in-python/ Code 2 Line 15
+        # Calculates the average of the contour points
+        # https://www.geeksforgeeks.org/numpy-mean-in-python/ Code 2 Line 15
         midpoint_x_arr = np.mean([contour[:, 0, :][:min_length][:, 0] for contour in contours], axis=0).astype(int)
         midpoint_y_arr = np.mean([contour[:, 0, :][:min_length][:, 1] for contour in contours], axis=0).astype(int)
 
-        #Displays the centerline
-        #https://www.geeksforgeeks.org/python-opencv-cv2-line-method/ Example 1 Line 33
+        # Displays the centerline
+        # https://www.geeksforgeeks.org/python-opencv-cv2-line-method/ Example 1 Line 33
         for i in range(len(midpoint_x_arr) - 1):
-            cv.line(image, (midpoint_x_arr[i], midpoint_y_arr[i]), (midpoint_x_arr[i+1], midpoint_y_arr[i+1]), (0, 0, 255), 5)
-
+            cv.line(image, (midpoint_x_arr[i], midpoint_y_arr[i]), (midpoint_x_arr[i + 1], midpoint_y_arr[i + 1]),
+                    (0, 0, 255), 5)
